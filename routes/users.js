96ea4321792396cf8aedb9f09874c80be2b22277
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-01-05 19:19:46 +0100

    register user  route

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,13 @@ router.get("/register", (req, res) => {
     res.render("users/register");
 })
 
+router.post("/register", async (req, res) => {
+    const { username, email, password } = req.body;//Destructuramos o que necesitamos do body da form
+    const user = new User({ email, username });
+    const registered = await User.register(user, password);
+    console.log(registered);
+    req.flash("Bienvenido a NoNo!");
+    res.redirect("/");
+})
+
 module.exports = router;
