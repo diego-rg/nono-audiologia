d7a32d42b1d84f248d6346d508b2c1ac458f3e48
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-01-09 17:31:26 +0100

    users routes restructured into controllers folder

---
+++ controllers/users.js
@@ -0,0 +1,48 @@
+const User = require("../models/user");
+
+//Register
+module.exports.registerForm = (req, res) => {
+    res.render("users/register");
+}
+
+module.exports.newUser = async (req, res) => {
+    try{
+        const { username, email, password } = req.body;//Destructuramos o que necesitamos do body da form
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if(err) return next(err);
+            req.flash("success", "Bienvenido a NoNo!");
+            res.redirect("/");
+        })
+    } catch(e) {//facemos un error handler extra para avisar se o nome de usuario xa está en uso e traducímolo
+        let translatedError;
+        if( e.message === "A user with the given username is already registered") {
+            translatedError = "Error: ya existe un usuario con ese nombre."
+            req.flash("error", translatedError);
+            res.redirect("register");
+        } else {
+           req.flash("error", e.message);
+            res.redirect("register"); 
+        }
+    }
+}
+
+//Login
+module.exports.loginForm = (req, res) => {
+    res.render("users/login");
+}
+
+module.exports.loginUser = (req, res) => {//Middleware de passport. Xa comproba él a authentication
+    req.flash("success", "Bienvenido a NoNo!");
+    const redirectUrl = req.session.returnTo || "/";//Usamos o redirectUrl creado no middleware.js para que o devolva a onde estaba ou a /
+    delete req.session.returnTo;//Eliminamos despois de usalo para que non de problemas
+    res.redirect(redirectUrl);//Rediriximos a url na que estaba despois de login
+}
+
+//Logout
+module.exports.logoutUser = (req, res) => {
+    req.logout();
+    req.flash("success","Has cerrado sesión." );
+    res.redirect("/");
+}
--- routes/users.js
+++ routes/users.js
@@ -4,52 +4,19 @@ const router = express.Router();
 const User = require("../models/user");
 const catchAsync = require("../utilities/catchAsync");
 const passport = require("passport");//Authentication
+const users = require("../controllers/users")
 
 //Register
-router.get("/register", (req, res) => {
-    res.render("users/register");
-})
+router.get("/register", users.registerForm);
 
-router.post("/register", catchAsync(async (req, res) => {
-    try{
-        const { username, email, password } = req.body;//Destructuramos o que necesitamos do body da form
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if(err) return next(err);
-            req.flash("success", "Bienvenido a NoNo!");
-            res.redirect("/");
-        })
-    } catch(e) {//facemos un error handler extra para avisar se o nome de usuario xa está en uso e traducímolo
-        let translatedError;
-        if( e.message === "A user with the given username is already registered") {
-            translatedError = "Error: ya existe un usuario con ese nombre."
-            req.flash("error", translatedError);
-            res.redirect("register");
-        } else {
-           req.flash("error", e.message);
-            res.redirect("register"); 
-        }
-    }
-}))
+router.post("/register", catchAsync(users.newUser));
 
 //Login
-router.get("/login", (req, res) => {
-    res.render("users/login");
-})
+router.get("/login", users.loginForm);
 
-router.post("/login", passport.authenticate("local", { failureFlash: "Los datos introducidos no son válidos.", failureRedirect: "/login" }), (req, res) => {//Middleware de passport. Xa comproba él a authentication
-    req.flash("success", "Bienvenido a NoNo!");
-    const redirectUrl = req.session.returnTo || "/";//Usamos o redirectUrl creado no middleware.js para que o devolva a onde estaba ou a /
-    delete req.session.returnTo;//Eliminamos despois de usalo para que non de problemas
-    res.redirect(redirectUrl);//Rediriximos a url na que estaba despois de login
-})
+router.post("/login", passport.authenticate("local", { failureFlash: "Los datos introducidos no son válidos.", failureRedirect: "/login" }), users.loginUser);
 
 //Logout
-router.get("/logout", (req, res) => {
-    req.logout();
-    req.flash("success","Has cerrado sesión." );
-    res.redirect("/");
-})
+router.get("/logout", users.logoutUser);
 
 module.exports = router;
